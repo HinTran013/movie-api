44b7f9bbeaf7ff0e80a2237b79bff04d68c44d40
HinTran013 <hienthanhh013@gmail.com>
2024-11-28 20:10:03 +0700

    setup swagger

---
+++ src/common/strings.ts
@@ -0,0 +1,4 @@
+export const STRINGS = {
+  SWAGGER_TITLE: 'Movie API',
+  SWAGGER_DESCRIPTION: 'The API for the movie',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { STRINGS } from './common/strings';
+import { APP_VERSION, SWAGGER_PATH } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(STRINGS.SWAGGER_TITLE)
+    .setDescription(STRINGS.SWAGGER_DESCRIPTION)
+    .setVersion(APP_VERSION)
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(SWAGGER_PATH, app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
